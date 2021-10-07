d06fe25d6e596f1946a1f3bdcd000eab1a209793
Piotr-Szyszkowski <peter.szyszkowski@protonmail.com>
2021-10-07 18:24:02 +0100

    some testing

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,7 +1,20 @@
+import { useState } from "react";
+
 const Home = () => {
+  const [color, colorSetter] = useState("green");
+  const toggleTextColor = () => {
+    if (color === "green") {
+      colorSetter("red");
+    } else {
+      colorSetter("green");
+    }
+  };
+
   return (
     <div className="Home">
       <img className="Home__Main-pic" src="/Images/me-in-rome.jpg" />
+      <p className={`temp-text-${color}`}>Text to change colour</p>
+      <button onClick={toggleTextColor}>Change color</button>
     </div>
   );
 };
