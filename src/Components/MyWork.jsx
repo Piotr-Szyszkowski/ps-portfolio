613acd60f33e8e2ffdded45372f2c135c6b39317
Piotr-Szyszkowski <peter.szyszkowski@protonmail.com>
2021-10-24 20:54:20 +0100

    started on MyWork page

--- src/Components/MyWork.jsx
+++ src/Components/MyWork.jsx
@@ -1,7 +1,25 @@
 const MyWork = () => {
   return (
-    <div>
-      <p>This will be My Work page</p>
+    <div className="MyWork">
+      <p className="MyWork__Title">NC-GAMES [back-end]</p>
+      <p className="MyWork__SectionText">
+        A fully functional RESTful API. Built with Node.js, PostgreSQL and
+        Express.js.
+      </p>
+      <a
+        className="MyWork__Link"
+        href="https://piotrs-games.herokuapp.com/api"
+        target="_blank"
+      >
+        Deployed version
+      </a>
+      <a
+        className="MyWork__Link"
+        href="https://github.com/Piotr-Szyszkowski/be-nc-games-public"
+        target="_blank"
+      >
+        Git Repository
+      </a>
     </div>
   );
 };
